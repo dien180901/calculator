cc24806cbec1d9e3545db2814eebf028adc2d9c8
Dien Ma Phu <s3891528@taas-iMac-61.RMIT.EDU.VN>
2021-12-15 10:36:32 +0700

    da

--- script.js
+++ script.js
@@ -2,154 +2,285 @@ let number = "0";
 let ans = 0;
 let is_first = true;
 let operator = "";
+let is_click_operator = false;
+let operators_id = {
+	"+": "#btn-plus",
+	"/": "#btn-div",
+	"-": "#btn-dec",
+	"*": "#btn-mul",
+};
+let operators_id_conver = {
+	"#btn-plus": "+",
+	"#btn-div": "/",
+	"#btn-dec": "-",
+	"#btn-mul": "*",
+};
 
 rerender();
 $("#btn-0").click(() => {
-  number += "0";
-  rerender();
+	number += "0";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-1").click(() => {
-  number += "1";
-  rerender();
+	number += "1";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-2").click(() => {
-  number += "2";
-  rerender();
+	number += "2";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-3").click(() => {
-  number += "3";
-  rerender();
+	number += "3";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-4").click(() => {
-  number += "4";
-  rerender();
+	number += "4";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-5").click(() => {
-  number += "5";
-  rerender();
+	number += "5";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-6").click(() => {
-  number += "6";
-  rerender();
+	number += "6";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-7").click(() => {
-  number += "7";
-  rerender();
+	number += "7";
+	rerender();
+
+	remove_disabled_button(operator);
 });
 $("#btn-8").click(() => {
-  number += "8";
-  rerender();
+	number += "8";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-9").click(() => {
-  number += "9";
-  rerender();
+	number += "9";
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
 $("#btn-point").click(() => {
-  if (number.indexOf(".") == -1) {
-    number += ".";
-    ans = parseFloat(number);
-  }
+	if (number.indexOf(".") == -1) {
+		number += ".";
 
-  rerender();
+	}
+
+	rerender();
 });
 $("#btn-ac").click(() => {
-  number = "0";
-  ans = parseFloat(number);
+	number = "0";
+	ans = parseFloat(number);
 
-  rerender();
+	rerender();
+	is_click_operator = false;
+	if (operator) {
+		remove_disabled_button(operators_id[operator]);
+	}
 });
-$("#btn-plus").click(() => {
-  if (operator) {
-    cal(operator);
+function disabled_button(except_operator) {
+	temp_operators = new Set([...Object.values(operators_id)]);
+  
+	temp_operators.delete(except_operator);
 
-    number = "0";
-  } else {
-    ans = parseFloat(number);
-    temp=ans
-    number = "0"
-    rerender();
-  }
-  operator = "+";
+	$(except_operator).addClass("hover-button-click ");
+	for (let i of temp_operators) {
+		$(i).prop("disabled", true);
+		$(i).removeClass("hover-button");
+	}
+}
+function remove_disabled_button(exception_operator) {
+	
+	temp_operators = new Set([...Object.values(operators_id)]);
+	temp_operators.delete(exception_operator);
+	$(exception_operator).removeClass("hover-button-click ");
+	$(exception_operator).prop("disabled", false);
+	for (let i of temp_operators) {
+		$(i).prop("disabled", false);
+		$(i).addClass("hover-button");
+	}
+}
+$("#btn-plus").click(() => {
+	if (!is_click_operator) {
+		if (operator) {
+			
+			cal(operator);
+
+			number = "0";
+		} else {
+			ans = parseFloat(number);
+			temp = ans;
+			number = "0";
+			rerender();
+		}
+		operator = "+";
+		is_click_operator = true;
+		disabled_button(operators_id["+"]);
+	} else {
+		if (operator == "+") {
+      temp=ans
+      number=ans.toString()
+			is_click_operator = false;
+			operator = "";
+			remove_disabled_button(operators_id["+"]);
+		}
+	}
 });
 $("#btn-dec").click(() => {
-  if (operator) {
-    cal(operator);
-    number = "0";
-  } else {
-    ans = parseFloat(number);
-    temp=ans
-    number = "0"
-    console.log(ans)
-    rerender();
-  }
+	if (!is_click_operator) {
+		if (operator) {
+			cal(operator);
 
-  operator = "-";
+			number = "0";
+		} else {
+			ans = parseFloat(number);
+			temp = ans;
+			number = "0";
+			rerender();
+		}
+		operator = "-";
+		is_click_operator = true;
+		disabled_button(operators_id["-"]);
+	} else {
+    
+		if (operator == "-") {
+      temp=ans
+      number=ans.toString()
+			is_click_operator = false;
+			operator = "";
+			remove_disabled_button(operators_id["-"]);
+		}
+	}
 });
 $("#btn-div").click(() => {
-  if (operator) {
-    cal(operator);
-    number = "0";
-  } else {
-    ans = parseFloat(number);
-    temp=ans
-    number = "0"
-    rerender();
-  }
-  operator = "/";
+  console.log("number div",number)
+	if (!is_click_operator) {
+		if (operator) {
+			cal(operator);
+
+			number = "0";
+		} else {
+			ans = parseFloat(number);
+			temp = ans;
+			number = "0";
+			rerender();
+		}
+		operator = "/";
+		is_click_operator = true;
+		disabled_button(operators_id["/"]);
+	} else {
+    
+    
+		if (operator == "/") {
+      temp=ans
+    number=ans.toString()
+			is_click_operator = false;
+			operator = "";
+			remove_disabled_button(operators_id["/"]);
+		}
+	}
 });
 $("#btn-mul").click(() => {
-  if (operator) {
-    cal(operator);
+	if (!is_click_operator) {
+		if (operator) {
+			cal(operator);
 
-    number = "0";
-  } else {
-    ans = parseFloat(number);
-    temp=ans
-    number = "0"
-    rerender();
-  }
-  operator = "*";
+			number = "0";
+		} else {
+			ans = parseFloat(number);
+			temp = ans;
+			number = "0";
+			rerender();
+		}
+		operator = "*";
+		is_click_operator = true;
+		disabled_button(operators_id["*"]);
+	} else {
+		remove_disabled_button(operators_id["*"]);
+		if (operator == "*") {
+      temp=ans
+    number=ans.toString()
+			is_click_operator = false;
+			operator = "";
+			remove_disabled_button(operators_id["*"]);
+		}
+	}
 });
 $("#btn-del").click(() => {
-  if (number != "0") {
-    temp = ans;
-    number = temp.toString();
-    number = number.slice(0, -1);
-    ans = parseFloat(number);
-    console.log(number);
-    console.log(ans);
-
-    rerender();
+  console.log("a",number)
+	if (number != "0") {
+		number = number.slice(0, -1);
+		rerender();
+		remove_disabled_button(operator);
+	}else if (number==""){
+    number="0"
   }
 });
 $("#btn-equal").click(() => {
-  cal(operator);
-  operator = "";
+	cal(operator);
+  
+	operator = "";
 });
 function cal(s) {
-  if (s == "+") {
-    ans = ans + parseFloat(number);
-  } else if (s == "-") {
-    ans = ans - parseFloat(number);
-  } else if (s == "*") {
-    ans = ans * parseFloat(number);
-  } else {
-    ans = ans / parseFloat(number);
-  }
-  number = ans.toString();
+	if (s == "+") {
+		ans = ans + parseFloat(number);
+	} else if (s == "-") {
+		ans = ans - parseFloat(number);
+	} else if (s == "*") {
+		ans = ans * parseFloat(number);
+	} else {
+		ans = ans / parseFloat(number);
+	}
+	number = ans.toString();
 
-  ans = parseFloat(ans);
-  console.log(ans)
+	ans = parseFloat(ans);
+	
 
-  rerender();
+	rerender();
 }
 
 function rerender() {
-    console.log(ans)
-  while (number[0] == "0" && number.length > 1) {
-    number = number.substring(1);
-  }
-  temp = parseFloat(number);
-  temp.toString();
-  document.getElementById("output").innerText = temp;
+	
+	while (number[0] == "0" && number.length > 1) {
+		number = number.substring(1);
+	}
+	temp = parseFloat(number);
+	temp.toString();
+	document.getElementById("output").innerText = temp;
 }
+
